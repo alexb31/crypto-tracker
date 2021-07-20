a93ff9b2c4d16405a7c354c405da292dab060edc
alexb31 <alexandrebornstein@gmail.com>
2021-07-20 13:50:40 +0200

    Updated Coin Component

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import Image from 'next/image'
+import styles from './Coins.module.css'
 
-const Coins = ({name}) => {
+const Coins = ({name, price, symbol, image, id, marketcap, volume, priceChange}) => {
     return (
-        <div>
-            <h1>
-                {name}
-            </h1>
+        <div className={styles.coin_container}>
+          <div className={styles.coin_row}>
+            <div className={styles.coin}>
+              <img src={image} alt={name} className={styles.coin_img} />
+              <h1 className={styles.coin_h1}>{name}</h1>
+              <p className={styles.coin_symbol}>{symbol}</p>
+            </div>
+            <div className={styles.coin_data}>
+              <p className={styles.coin_price}>${price}</p>
+              <p className={styles.coin_volume}>${volume.toLocaleString()}</p>
+
+              {priceChange < 0 ? (
+                <p className={(styles.coin_percent, styles.red)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              ) : (
+                <p className={(styles.coin_percent, styles.green)}>
+                  {priceChange.toFixed(2)}%
+                </p>
+              )}
+
+              <p className={styles.coin_marketcap}>
+                Mkt Cap: ${marketcap.toLocaleString()}
+              </p>
+            </div>
+          </div>
         </div>
     )
 }
